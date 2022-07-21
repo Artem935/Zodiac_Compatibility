afddceb8833ad4a5835b4f4311bdfb19f846939f
Artem935 <72891194+Artem935@users.noreply.github.com>
2022-07-21 16:54:49 +0300

    V 0.2.0

---
+++ Zodiac_Compatibility/AboutYou.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zodiac_Compatibility
+{
+    /// <summary>
+    /// Логика взаимодействия для ServiceSelection.xaml
+    /// </summary>
+    public partial class ServiceSelection : Page
+    {
+        SqlConnection sqlConnection = null;
+        public int Id;
+        public ServiceSelection(int Id)
+        {
+            Main main = new Main();
+            Finder finder = new Finder();
+            InitializeComponent();
+            Text(Id);
+            ColorChengerText(AboutYou1);
+            ColorChengerText(AboutYou);
+        }
+        private void ReturnMain_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Main());
+        }
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Text(int id)
+        {
+            string LanguageShort;
+            string LanguageLong;
+            if (Settings.Eng)
+            {
+                LanguageShort = "ShortDescriptionENG";
+                LanguageLong = "LongDescriptionENG";
+            }
+            else
+            {
+                LanguageShort = "ShortDescriptionUKR";
+                LanguageLong = "LongDescriptionUKR";
+            }
+                
+
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ZodiacDB"].ConnectionString);
+            sqlConnection.Open();
+            SqlDataReader dataReader = null;
+
+            SqlCommand cmd1 = new SqlCommand($"SELECT {LanguageShort} FROM Text WHERE id = {id}", sqlConnection);
+            dataReader = cmd1.ExecuteReader();
+            while (dataReader.Read())
+                AboutYou1.Text = dataReader[$"{LanguageShort}"].ToString();
+            dataReader.Close();
+
+            SqlCommand cmd2 = new SqlCommand($"SELECT {LanguageLong} FROM Text WHERE id = {id}", sqlConnection);
+            dataReader = cmd2.ExecuteReader();
+            while (dataReader.Read())
+                AboutYou.Text = dataReader[$"{LanguageLong}"].ToString();
+            dataReader.Close();
+        }
+        
+        async void ColorChengerText(TextBlock textBlock)
+        {
+            for (byte r = 35,g = 35,b = 62; r < 255 & g < 255 & b < 255; r += 10, g += 10, b += 9,await Task.Delay(25))
+            {
+                textBlock.Foreground = new SolidColorBrush(Color.FromRgb(r, g, b));
+            }
+        }
+    }
+}
---
+++ Zodiac_Compatibility/Main.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace Zodiac_Compatibility
+{
+
+    public partial class Main : Page
+    {
+        SqlConnection sqlConnection = null;
+        public Main()
+        {
+            Random rund = new Random();
+            InitializeComponent();
+            Day.Text = "10";
+            Month.Text = "7";
+            Year.Text = "2003";
+
+            if (!Settings.Eng)
+            {
+                DayLabel.Content = "День";
+                MonthLabel.Content = "Місяць";
+                YearLabel.Content = "Рік";
+                NameLabel.Content = "Ім'я";
+            }
+        }
+        private void UserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (UserName.Text.Length == 0)
+            {
+                UserName.Text = " ";
+                UserName.Select(UserName.Text.Length, 0);
+            }
+        }
+        private void UserName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"\p{L}");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+        private void UserName_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+        private void Day_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            ChekBox chekBox = new ChekBox();
+            chekBox.ChekDay(this, e);
+        }
+        private void Day_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+        private void Month_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            ChekBox chekBox = new ChekBox(); 
+            chekBox.ChekMonth(this,e);
+        }
+        private void Month_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+        private void Month_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Month.Text))
+            {
+                sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ZodiacDB"].ConnectionString);
+                sqlConnection.Open();
+                SqlDataReader dataReader = null;
+                SqlCommand cmdSel = new SqlCommand($"SELECT Day FROM Calendar WHERE id = {Convert.ToInt32(Month.Text)}", sqlConnection);
+                dataReader = cmdSel.ExecuteReader();
+                while (dataReader.Read())
+                    if (Convert.ToInt32(Day.Text) > Convert.ToInt32(dataReader["Day"].ToString()))
+                    {
+                        Day.Text = Convert.ToInt32(dataReader["Day"].ToString()).ToString();
+                    }
+                dataReader.Close();
+            }
+        }
+        private void Year_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            ChekBox chekBox = new ChekBox();
+            chekBox.ChekYear(this, e);
+        }
+        private void Year_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+        
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            Finder finder = new Finder();
+            int Id = finder.ZodiacIdFinder(this);
+            NavigationService.Navigate(new ServiceSelection(Id));
+        }
+    }
+}
